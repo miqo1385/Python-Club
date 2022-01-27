40b0ecb1c1da2a4a490ad85d205fa626ba1b1491
Miguel Quintana <miqo1385@gmail.com>
2022-01-26 21:37:50 -0800

    change to views

--- techreview/tech/views.py
+++ techreview/tech/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'tech/index.html' )
+    return render(request, 'techreview/tech/index.html' )
