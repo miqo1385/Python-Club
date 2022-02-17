c9df6b6b22e6bfb8d40c04b56f2f59709a9c3a72
Miguel Quintana <miqo1385@gmail.com>
2022-02-16 22:54:53 -0800

    detail commit

--- techreview/tech/views.py
+++ techreview/tech/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductType, Review 
 
 # Create your views here.
@@ -9,3 +9,7 @@ def index(request):
 def products(request):
     product_list=Product.objects.all()
     return render(request, 'tech/products.html', {'product_list': product_list})
+
+def productDetail(request, id):
+    product=get_object_or_404(Product, pk=id)
+    return render(request, 'tech/productdetail.html', {'product' : product})
--- techreview/techreview/urls.py
+++ techreview/techreview/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tech/', include('tech.urls')),
+    path('productdetail/<int:id>', views.productdetail, name='detail'),
 ]
  
